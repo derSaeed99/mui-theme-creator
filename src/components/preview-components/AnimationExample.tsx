5cb18dff769c92405685312c57c2ca4010d0c7c3
Saeed <104280610+derSaeed99@users.noreply.github.com>
2024-12-29 16:48:12 +0100

    fixup

--- src/components/preview-components/AnimationExample.tsx
+++ src/components/preview-components/AnimationExample.tsx
@@ -1,15 +1,9 @@
-import { Button } from '@mui/material';
-import {Animate } from '../hooks/useAnimation';
+import { Button } from "@mui/material";
+import { Animate } from "../../hooks/useAnimation";
 export const AnimationExample = () => {
-    return (
-        <Animate
-            animation="popBounce"
-            trigger="onClick"
-            duration={800}
-        >
-            <Button variant="contained">
-                Click me!
-            </Button>
-        </Animate>
-    );
-}
+  return (
+    <Animate animation="popBounce" trigger="onClick" duration={800}>
+      <Button variant="contained">Click me!</Button>
+    </Animate>
+  );
+};
